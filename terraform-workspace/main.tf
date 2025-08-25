e891a919cf16c1ac74c37ec3e364b2d7d3b06955
Madhurima975 <115366302+Madhurima975@users.noreply.github.com>
2025-08-25 20:29:24 +0530

    Create main.tf

---
+++ terraform-workspace/main.tf
@@ -0,0 +1,22 @@
+variable "ami" {
+  description = "This is AMI for the instance"
+}
+
+variable "instance_type" {
+  description = "This is the instance type, for example: t2.micro"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID for the EC2 instance"
+  type        = string
+}
+
+
+
+resource "aws_instance" "example" {
+    ami = var.ami
+    instance_type = var.instance_type
+    subnet_id     = var.subnet_id   # required because no default VPC
+
+
+}
